ec9f3cf9a3b174573be96eb6881a935b7bb0125e
Ismail Larhrib <ilarhrib@e3r3p13.1337.ma>
2026-01-06 16:24:09 +0100

    ...

--- Cpp5-9/Cpp07/ex02/main.cpp
+++ Cpp5-9/Cpp07/ex02/main.cpp
@@ -1,26 +1,53 @@
+#include <iostream>
 #include "Array.hpp"
 
+#define MAX_VAL 750
+int main(int, char**)
+{
+    Array<int> numbers(MAX_VAL);
+    int* mirror = new int[MAX_VAL];
+    srand(time(NULL));
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        const int value = rand();
+        numbers[i] = value;
+        mirror[i] = value;
+    }
+    //SCOPE
+    {
+        Array<int> tmp = numbers;
+        Array<int> test(tmp);
+    }
 
-int main(){
-	Array<int> copy_array(10);
-	for (unsigned int i = 0;i < copy_array.size();i++){
-		copy_array[i] = i;
-	}
-	Array<int> int_array(copy_array);
-	for (unsigned int i = 0;i < int_array.size();i++){
-		std::cout << "[" << int_array[i] << "]" << std::endl;
-	}
-	// exception error
-	Array<int> array(10);
-	for (unsigned int i = 0;i < array.size();i++){
-		array[i] = i + 1;
-	}
-	try {
-		array[999];
-	}
-	catch(std::runtime_error &excep){
-		std::cout << excep.what() << std::endl;
-	}
-	std::cout << array[9] << std::endl;
-	return (0);
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        if (mirror[i] != numbers[i])
+        {
+            std::cerr << "didn't save the same value!!" << std::endl;
+            return 1;
+        }
+    }
+    try
+    {
+        numbers[-2] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    try
+    {
+        numbers[MAX_VAL] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        numbers[i] = rand();
+    }
+    delete [] mirror;//
+    return 0;
 }
---
+++ Cpp5-9/Cpp08/easyfind.hpp
@@ -0,0 +1,6 @@
+//#ifndef EASYFIND_HPP
+//#define EASYFIND_HPP
+
+//template <typename T> void easyfind(T, )
+
+//#endif
