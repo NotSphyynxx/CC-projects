b78c651245d8b8559ce5c594384a2101a7898539
Sphynx <thesphynx48@gmail.com>
2026-07-24 11:06:58 +0100

    v1

--- Cpp5-9/Cpp08/ex01/Span.cpp
+++ Cpp5-9/Cpp08/ex01/Span.cpp
@@ -4,6 +4,10 @@ const char* CapIsFull::what() const throw() {
     return "Array capacity is full !!";
 }
 
+const char* NotEnoughNum::what() const throw() {
+    return "Not enough numbers !!";
+}
+
 Span::Span() : N(0) {
 
 }
@@ -28,9 +32,42 @@ Span &Span::operator=(const Span &obj) {
     return *this;
 }
 
-void Span::addNumber(unsigned int num){
-    // if (num > )
+void Span::addNumber(int num){
     if (depot.size() + 1 > N)
         throw CapIsFull();
     depot.push_back(num);
+    std::sort(depot.begin(), depot.end());
+}
+
+int Span::longestSpan() {
+    if (depot.size() < 2)
+        throw NotEnoughNum();
+    return (*(depot.end() - 1) - *depot.begin()); 
+}
+
+int Span::shortestSpan() {
+    if (depot.size() < 2)
+        throw NotEnoughNum();
+    std::vector<int>::iterator curr = depot.begin();
+    std::vector<int>::iterator next = depot.begin();
+    ++next;
+    int shortestYet = *next - *curr;
+    while (next != depot.end()){
+        int tmp = *next - *curr;
+        if (tmp < shortestYet)
+            shortestYet = tmp;
+        ++curr;
+        ++next;
+    }
+    return shortestYet;
+}
+
+void Span::addMultiNumbers(std::vector<int>::iterator begin, std::vector<int>::iterator end){
+    while (begin != end){
+        if (depot.size() + 1 > N)
+            throw CapIsFull();
+        depot.push_back(*begin);
+        ++begin;
+    }
+    std::sort(depot.begin(), depot.end());
 }
